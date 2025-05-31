74665d2dd6beeb754347c1e9c3a8ff527471a507
Taís Colares <115088713+taiscolares@users.noreply.github.com>
2025-05-31 18:41:14 -0300

    Add files via upload

---
+++ FormasGeometricas-Encapsuladas/FormasGeometricas/src/br/edu/formasgeométricasplanas/Triangulo.java
@@ -0,0 +1,29 @@
+
+package br.edu.formasgeométricasplanas;
+public class Triangulo {
+    private double base;
+    private double altura;
+    private double area;
+
+    public Triangulo() {
+    	base = 10;
+    	altura = 10;
+    	area = 0;
+    	
+    } public void setBase(double pBase) {
+        base = base;
+    } public double getBase() {
+        return base;
+    } public void setAltura(double pAltura) {
+        altura = altura;
+    } public double getAltura() {
+        return altura;
+    } public void calcArea() {
+        area = (base * altura) / 2;
+    } public void mostrarArea() {
+        System.out.println("Área: " + area);
+    } public double getArea() {
+        System.out.println("Área com retorno: " + area);
+        return area;
+    }
+}
